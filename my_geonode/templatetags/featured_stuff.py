125d0a8f48cddef4fb72d94d57e1d0cced7e05bf
Michael Wagner <mwagner@allspatial.info>
2016-07-24 10:55:42 +0400

    Added feature to show recently added layers and maps

--- my_geonode/templatetags/featured_stuff.py
+++ my_geonode/templatetags/featured_stuff.py
@@ -1,5 +1,6 @@
 from django import template
 from geonode.layers.models import Layer
+from geonode.maps.models import Map
 from geonode import settings
 from guardian.shortcuts import get_objects_for_user
 
@@ -7,7 +8,7 @@ register = template.Library()
 
 
 @register.assignment_tag(takes_context=True)
-def last_three_layers(context):
+def lastest_layers(context):
 
     request = context['request']
 
@@ -23,7 +24,7 @@ def last_three_layers(context):
     if not settings.SKIP_PERMS_FILTER:
         layers = layers.filter(id__in=authorized)
 
-    layers = layers[:5]
+    layers = layers[:3]
 
     layer_list = list()
 
@@ -39,3 +40,39 @@ def last_three_layers(context):
         layer_list.append(featured_layer)
 
     return layer_list
+
+
+@register.assignment_tag(takes_context=True)
+def featured_maps(context):
+
+    request = context['request']
+
+    maps = Map.objects.order_by('-popular_count')
+
+    if settings.RESOURCE_PUBLISHING:
+        maps = maps.filter(is_published=True)
+
+    if not settings.SKIP_PERMS_FILTER:
+        authorized = get_objects_for_user(
+            request.user, 'base.view_resourcebase').values('id')
+
+    if not settings.SKIP_PERMS_FILTER:
+        maps = maps.filter(id__in=authorized)
+
+    maps = maps[:3]
+
+    map_list = list()
+
+    for map in maps:
+        featured_map = dict()
+        featured_map['title'] = map.title
+        modified_timestamp = map.last_modified
+        featured_map['date'] = modified_timestamp.date()
+        featured_map['owner'] = map.owner
+        featured_map['thumbnail_url'] = map.thumbnail_url
+        featured_map['view_url'] = map.detail_url + "/view"
+        featured_map['abstract'] = map.abstract
+        featured_map['popular_count'] = map.popular_count
+        map_list.append(featured_map)
+
+    return map_list
